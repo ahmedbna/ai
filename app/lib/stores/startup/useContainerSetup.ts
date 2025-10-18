f731a2c586c438389b44c8387c9eedf82bf795a6
ahmedbna <hi@ahmedbna.com>
2025-10-18 16:31:19 +0300

    snapshot

--- app/lib/stores/startup/useContainerSetup.ts
+++ app/lib/stores/startup/useContainerSetup.ts
@@ -23,7 +23,7 @@ import { chatSyncState } from './chatSyncState';
 import { FILE_EVENTS_DEBOUNCE_MS } from '@/lib/stores/files';
 import { setChefDebugProperty } from 'bna-agent/utils/chefDebug';
 
-const TEMPLATE_URL = '/template-snapshot-6fa7f980.bin';
+const TEMPLATE_URL = '/template-snapshot-b2811b17.bin';
 
 export function useNewChatContainerSetup() {
   const convex = useConvex();
